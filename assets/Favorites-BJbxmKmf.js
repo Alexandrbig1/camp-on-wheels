bcccf77bcb4254b527ae4146fb2b1c80b79d9e3c
Alexandrbig1 <alexsmagin1@gmail.com>
2024-04-16 17:03:30 -0500

    Updates

--- assets/Favorites-BJbxmKmf.js
+++ assets/Favorites-BJbxmKmf.js
@@ -1,4 +1,4 @@
-import{u as r,p as l,r as s,n as h,z as g,j as e,H as f,l as u,s as v}from"./index-Bq8Lr0ij.js";import{C as x,v as y}from"./v4-CRA9_EJa.js";import{h as j}from"./heroBg-BCOlliND.js";import"./StarRating-BseSgEDV.js";const b=r.div`
+import{u as r,p as l,r as s,n as h,z as g,j as e,H as f,l as u,s as v}from"./index-BV-n6NK6.js";import{C as x,v as y}from"./v4-XLpc_dy4.js";import{h as j}from"./heroBg-BCOlliND.js";import"./StarRating-8Z61T8-K.js";const b=r.div`
   display: flex;
   flex-direction: column;
   gap: 5rem;
