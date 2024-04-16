cc84029d675de69e48297d35fd1c277e0b485f50
Alexandrbig1 <alexsmagin1@gmail.com>
2024-04-16 12:39:20 -0500

    Updates

--- assets/Privacy.styled-BTFYm6aY.js
+++ assets/Privacy.styled-BTFYm6aY.js
@@ -1,4 +1,4 @@
-import{u as i,p as t}from"./index-GVjJqcPF.js";import{h as o}from"./heroBg-BCOlliND.js";const a=i.div`
+import{u as i,p as t}from"./index-Dh_S_Zqt.js";import{h as o}from"./heroBg-BCOlliND.js";const a=i.div`
   min-height: 100vh;
   margin: 0 auto;
 `,l=i.div`
