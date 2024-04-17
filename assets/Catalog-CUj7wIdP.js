b3314eeddda58f854797edf12eed3df5f74cea7b
Alexandrbig1 <alexsmagin1@gmail.com>
2024-04-16 22:47:01 -0500

    Updates

--- assets/Catalog-CUj7wIdP.js
+++ assets/Catalog-CUj7wIdP.js
@@ -1,4 +1,4 @@
-import{u as r,p as m,G as V,i as y,m as W,r as l,n as q,j as o,o as R,q as M,T as k,s as I,H as D,l as G,t as K}from"./index-DZ4gwkl3.js";import{M as N,C as z,v as O}from"./v4-BLnXi9Ek.js";import{b as Z}from"./StarRating-CFwgiZP3.js";const J=e=>e.cars.cars.items,Q=e=>e.cars.filter,U=r.div`
+import{u as r,p as s,G as W,i as j,m as M,r as a,n as q,j as o,o as R,q as D,T as k,s as I,H as G,l as K,t as N}from"./index-C0GlBgbr.js";import{M as Z,C as z,v as B}from"./v4-I325STq1.js";import{b as J}from"./StarRating-eloG7IuR.js";const Q=e=>e.cars.cars.items,U=e=>e.cars.filter,X=r.div`
   display: flex;
   gap: 6.4rem;
   align-items: start;
@@ -8,7 +8,12 @@ import{u as r,p as m,G as V,i as y,m as W,r as l,n as q,j as o,o as R,q as M,T a
   /* @media (min-width: 1440px) {
     padding: 9.6rem 12.8rem;
   } */
-`,X=r.ul`
+`,Y=r.div`
+  display: flex;
+  flex-direction: column;
+  gap: 5rem;
+  align-items: center;
+`,_=r.ul`
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -36,13 +41,13 @@ import{u as r,p as m,G as V,i as y,m as W,r as l,n as q,j as o,o as R,q as M,T a
     row-gap: 5rem;
     column-gap: 2.4rem;
   } */
-`,Y=r.li`
-  font-family: ${m};
+`,ee=r.li`
+  font-family: ${s};
   font-weight: 500;
   font-size: 16px;
   line-height: 1.5;
   color: ${e=>e.theme.colors.mainTextColor};
-`,S=["Chicago, IL","Aurora, IL","Rosemont, IL","Schaumburg, IL","Joliet, IL","Rockford, IL","Oak Park, IL","Bolingbrook, IL","Orland Park, IL","Skokie, IL","Peoria, IL"];function _(e){return V({tag:"svg",attr:{viewBox:"0 0 256 256",fill:"currentColor"},child:[{tag:"path",attr:{d:"M216,64H147.31l34.35-34.34a8,8,0,1,0-11.32-11.32L128,60.69,85.66,18.34A8,8,0,0,0,74.34,29.66L108.69,64H40A16,16,0,0,0,24,80V200a16,16,0,0,0,16,16H216a16,16,0,0,0,16-16V80A16,16,0,0,0,216,64Zm0,136H40V80H216V200Z"},child:[]}]})(e)}const ee=r.form`
+`,S=["Chicago, IL","Aurora, IL","Rosemont, IL","Schaumburg, IL","Joliet, IL","Rockford, IL","Oak Park, IL","Bolingbrook, IL","Orland Park, IL","Skokie, IL","Peoria, IL"];function oe(e){return W({tag:"svg",attr:{viewBox:"0 0 256 256",fill:"currentColor"},child:[{tag:"path",attr:{d:"M216,64H147.31l34.35-34.34a8,8,0,1,0-11.32-11.32L128,60.69,85.66,18.34A8,8,0,0,0,74.34,29.66L108.69,64H40A16,16,0,0,0,24,80V200a16,16,0,0,0,16,16H216a16,16,0,0,0,16-16V80A16,16,0,0,0,216,64Zm0,136H40V80H216V200Z"},child:[]}]})(e)}const re=r.form`
   display: flex;
   flex-direction: column;
   gap: 1.8rem;
@@ -54,16 +59,16 @@ import{u as r,p as m,G as V,i as y,m as W,r as l,n as q,j as o,o as R,q as M,T a
   @media (min-width: 1440px) {
     gap: 6.4rem;
   }
-`,oe=r.div`
+`,te=r.div`
   display: flex;
   flex-direction: column;
   gap: 3.2rem;
-`,B=r.div`
+`,H=r.div`
   background-color: ${e=>e.theme.colors.filtersLine};
   width: 100%;
   height: 2px;
   margin: 1rem 0 1rem;
-`,re=r.div`
+`,ne=r.div`
   display: flex;
   flex-direction: column;
   gap: 0.8rem;
@@ -118,7 +123,7 @@ import{u as r,p as m,G as V,i as y,m as W,r as l,n as q,j as o,o as R,q as M,T a
   @media (min-width: 860px) {
     padding: 1.4rem 4.4rem;
   }
-`;const te=r.div`
+`;const ie=r.div`
   position: relative;
   border: none;
   outline: none;
@@ -134,7 +139,7 @@ import{u as r,p as m,G as V,i as y,m as W,r as l,n as q,j as o,o as R,q as M,T a
   align-items: center;
   gap: 0.8rem;
 
-  font-family: ${m};
+  font-family: ${s};
   font-weight: 500;
   font-size: 1.6rem;
   line-height: 1.11111;
@@ -161,7 +166,7 @@ import{u as r,p as m,G as V,i as y,m as W,r as l,n as q,j as o,o as R,q as M,T a
   @media (min-width: 1000px) {
     width: 22.4rem;
   } */
-`,ne=r.div`
+`,le=r.div`
   display: flex;
   flex-direction: column;
   width: 100%;
@@ -197,8 +202,8 @@ import{u as r,p as m,G as V,i as y,m as W,r as l,n as q,j as o,o as R,q as M,T a
   display: flex;
   flex-direction: column;
   gap: 0.8rem;
-`,ie=r.span`
-  font-family: ${m};
+`,ae=r.span`
+  font-family: ${s};
   font-weight: 500;
   font-size: 16px;
   line-height: 1.25;
@@ -209,46 +214,46 @@ import{u as r,p as m,G as V,i as y,m as W,r as l,n as q,j as o,o as R,q as M,T a
     cursor: pointer;
     color: ${e=>e.theme.colors.mainTextColor};
   }
-`,le=r.div`
+`,se=r.div`
   display: flex;
   flex-wrap: wrap;
   column-gap: 1rem;
   row-gap: 0.8rem;
   width: 362px;
-`,ae=r.div`
+`,ce=r.div`
   display: flex;
   flex-wrap: wrap;
   column-gap: 1rem;
   row-gap: 0.8rem;
   width: 362px;
-`,E=r.div`
+`,O=r.div`
   display: flex;
   flex-direction: column;
   gap: 1.4rem;
-`,H=r.h3`
-  font-family: ${y};
+`,E=r.h3`
+  font-family: ${j};
   font-weight: 600;
   font-size: 20px;
   line-height: 1.2;
   color: ${e=>e.theme.colors.mainTextColor};
-`,se=r.span`
-  font-family: ${y};
+`,de=r.span`
+  font-family: ${j};
   font-weight: 500;
   font-size: 16px;
   line-height: 1.5;
   color: ${e=>e.theme.colors.secondaryTextColor};
-`,s=r.span`
-  font-family: ${y};
+`,c=r.span`
+  font-family: ${j};
   font-weight: 500;
   font-size: 16px;
   line-height: 1.25;
   text-align: center;
   color: ${e=>e.theme.colors.mainTextColor};
-`,c=r.span`
+`,d=r.span`
   font-size: 3.2rem;
   color: ${e=>e.theme.colors.mainTextColor};
   pointer-events: none;
-`,d=r.div`
+`,m=r.div`
   border: 1px solid
     ${e=>e!=null&&e.$active?e.theme.colors.accentColor:e.theme.colors.borderFilterItem};
   border-radius: 10px;
@@ -267,8 +272,8 @@ import{u as r,p as m,G as V,i as y,m as W,r as l,n as q,j as o,o as R,q as M,T a
     border: 1px solid ${e=>e.theme.colors.accentColor};
     cursor: pointer;
   }
-`,ce=r.label`
-  font-family: ${m};
+`,me=r.label`
+  font-family: ${s};
   font-weight: 500;
   font-size: 14px;
   line-height: 1.28571;
@@ -286,7 +291,7 @@ import{u as r,p as m,G as V,i as y,m as W,r as l,n as q,j as o,o as R,q as M,T a
   background-color: ${e=>e.theme.colors.inputBgColor};
   padding: 1.4rem 2.4rem;
 
-  font-family: ${m};
+  font-family: ${s};
   font-weight: 500;
   font-size: 1.6rem;
   line-height: 1.11111;
@@ -299,7 +304,7 @@ import{u as r,p as m,G as V,i as y,m as W,r as l,n as q,j as o,o as R,q as M,T a
   }
 
   &::placeholder {
-    font-family: ${m};
+    font-family: ${s};
     font-weight: 500;
     font-size: 1.6rem;
     line-height: 1.11111;
@@ -325,7 +330,7 @@ import{u as r,p as m,G as V,i as y,m as W,r as l,n as q,j as o,o as R,q as M,T a
   background-color: ${e=>e.theme.colors.inputBgColor};
   padding: 1.4rem 2.4rem;
 
-  font-family: ${m};
+  font-family: ${s};
   font-weight: 500;
   font-size: 1.6rem;
   line-height: 1.11111;
@@ -338,7 +343,7 @@ import{u as r,p as m,G as V,i as y,m as W,r as l,n as q,j as o,o as R,q as M,T a
   }
 
   &::placeholder {
-    font-family: ${m};
+    font-family: ${s};
     font-weight: 500;
     font-size: 1.6rem;
     line-height: 1.11111;
@@ -358,7 +363,7 @@ import{u as r,p as m,G as V,i as y,m as W,r as l,n as q,j as o,o as R,q as M,T a
   position: absolute;
   bottom: 0;
   left: 0;
-`;const de=r(W)`
+`;const he=r(M)`
   font-size: 1.8rem;
   color: ${e=>e.theme.colors.mainTextColor};
 
@@ -369,7 +374,7 @@ import{u as r,p as m,G as V,i as y,m as W,r as l,n as q,j as o,o as R,q as M,T a
   @media (min-width: 1440px) {
     right: 1.2rem;
   }
-`,me=r.button`
+`,pe=r.button`
   border: none;
   outline: none;
   background: none;
@@ -378,7 +383,7 @@ import{u as r,p as m,G as V,i as y,m as W,r as l,n as q,j as o,o as R,q as M,T a
   width: 173px;
   height: 56px;
 
-  font-family: ${y};
+  font-family: ${j};
   font-weight: 500;
   font-size: 16px;
   line-height: 1.5;
@@ -392,4 +397,29 @@ import{u as r,p as m,G as V,i as y,m as W,r as l,n as q,j as o,o as R,q as M,T a
     background-color: ${e=>e.theme.colors.accentColorHover};
     cursor: pointer;
   }
-`;function he({handlePage:e,setDisplayedCars:u,setFilteredSearch:T}){const[C,p]=l.useState(""),g=l.useRef(null),[$,f]=l.useState(!1),[h,b]=l.useState(""),[i,w]=l.useState("");q();const F=t=>{const x=t.target.innerText;b(x)},L=t=>{const x=t.target.innerText;w(x)},j=t=>{p(t),f(!1)},v=async()=>{b(""),w(""),p("")},n=()=>{v(),e()};async function a(t){t.preventDefault(),g.current.innerText,n()}return o.jsxs(ee,{onSubmit:a,children:[o.jsxs(oe,{children:[o.jsxs(re,{children:[o.jsx(ce,{htmlFor:"location",children:"Location"}),o.jsxs(te,{ref:g,onClick:()=>f(!$),children:[o.jsx(de,{}),o.jsx("span",{children:C||"Location"})]}),$&&o.jsx(ne,{children:S==null?void 0:S.map(t=>o.jsx(ie,{onClick:()=>j(t),children:t},t))})]}),o.jsxs(E,{children:[o.jsx(se,{children:"Filters"}),o.jsx(H,{children:"Vehicle equipment"}),o.jsx(B,{}),o.jsxs(le,{onClick:F,children:[o.jsxs(d,{$active:h==="AC",children:[o.jsx(c,{children:o.jsx(N,{})}),o.jsx(s,{children:"AC"})]}),o.jsxs(d,{$active:h==="Automatic",children:[o.jsx(c,{children:o.jsx(R,{})}),o.jsx(s,{children:"Automatic"})]}),o.jsxs(d,{$active:h==="Kitchen",children:[o.jsx(c,{children:o.jsx(M,{})}),o.jsx(s,{children:"Kitchen"})]}),o.jsxs(d,{$active:h==="TV",children:[o.jsx(c,{children:o.jsx(_,{})}),o.jsx(s,{children:"TV"})]}),o.jsxs(d,{$active:h==="Shower/WC",children:[o.jsx(c,{children:o.jsx(Z,{})}),o.jsx(s,{children:"Shower/WC"})]})]})]}),o.jsxs(E,{children:[o.jsx(H,{children:"Vehicle type"}),o.jsx(B,{}),o.jsxs(ae,{onClick:L,children:[o.jsxs(d,{$active:i==="Van",children:[o.jsx(c,{children:o.jsx(k,{})}),o.jsx(s,{children:"Van"})]}),o.jsxs(d,{$active:i==="Fully Integrated",children:[o.jsx(c,{children:o.jsx(k,{})}),o.jsx(s,{children:"Fully Integrated"})]}),o.jsxs(d,{$active:i==="Alcove",children:[o.jsx(c,{children:o.jsx(k,{})}),o.jsx(s,{children:"Alcove"})]})]})]})]}),o.jsx(me,{children:"Search"})]})}function ge(){const e=q(),u=I(J),[T,C]=l.useState(!1),[p,g]=l.useState(1),[$,f]=l.useState(!0),[h,b]=l.useState([]),i=I(Q);function w(){g(1)}const j=Math.ceil(52/12);l.useEffect(()=>{async function a(){try{f(p<j),await e(K(p)).unwrap()}catch(t){console.log(t.message)}}a()},[e,p,j]);function v(){return h.filter(t=>{const x=!i.brand||t.make===i.brand,A=parseInt(t.rentalPrice.replace("$",""))<=i.price,P=!i.mileageRange||t.mileage>=i.mileageRange.min&&t.mileage<=i.mileageRange.max;return x&&A&&P})}const n=v();return o.jsxs(o.Fragment,{children:[o.jsx(D,{children:o.jsxs(G,{children:[o.jsx("title",{children:"Cruise Wheels - Explore the Catalog"}),o.jsx("meta",{name:"description",content:"Browse through our diverse catalog of stylish and comfortable cars at Cruise Wheels. Find the perfect vehicle for your next adventure and experience the joy of premium car rentals."}),o.jsx("meta",{name:"keywords",content:"car catalog, car rental, stylish cars, comfortable journeys, Cruise Wheels, premium rental services"})]})}),o.jsxs(U,{children:[o.jsx(he,{handlePage:w,setDisplayedCars:b,setFilteredSearch:C}),o.jsx(X,{children:n&&(n==null?void 0:n.length)===0&&T?o.jsx(Y,{children:"No matching cars found"}):(n==null?void 0:n.length)>0?n==null?void 0:n.map(a=>o.jsx(z,{items:a},O())):u==null?void 0:u.map(a=>o.jsx(z,{items:a},O()))})]})]})}export{ge as default};
+`;function xe({handlePage:e,setDisplayedCars:x,setFilteredSearch:L}){const[y,p]=a.useState(""),u=a.useRef(null),[C,g]=a.useState(!1),[h,f]=a.useState(""),[i,b]=a.useState("");q();const $=t=>{const l=t.target.innerText;f(l)},T=t=>{const l=t.target.innerText;b(l)},F=t=>{p(t),g(!1)},w=async()=>{f(""),b(""),p("")},v=()=>{w(),e()};async function n(t){t.preventDefault(),u.current.innerText,v()}return o.jsxs(re,{onSubmit:n,children:[o.jsxs(te,{children:[o.jsxs(ne,{children:[o.jsx(me,{htmlFor:"location",children:"Location"}),o.jsxs(ie,{ref:u,onClick:()=>g(!C),children:[o.jsx(he,{}),o.jsx("span",{children:y||"Location"})]}),C&&o.jsx(le,{children:S==null?void 0:S.map(t=>o.jsx(ae,{onClick:()=>F(t),children:t},t))})]}),o.jsxs(O,{children:[o.jsx(de,{children:"Filters"}),o.jsx(E,{children:"Vehicle equipment"}),o.jsx(H,{}),o.jsxs(se,{onClick:$,children:[o.jsxs(m,{$active:h==="AC",children:[o.jsx(d,{children:o.jsx(Z,{})}),o.jsx(c,{children:"AC"})]}),o.jsxs(m,{$active:h==="Automatic",children:[o.jsx(d,{children:o.jsx(R,{})}),o.jsx(c,{children:"Automatic"})]}),o.jsxs(m,{$active:h==="Kitchen",children:[o.jsx(d,{children:o.jsx(D,{})}),o.jsx(c,{children:"Kitchen"})]}),o.jsxs(m,{$active:h==="TV",children:[o.jsx(d,{children:o.jsx(oe,{})}),o.jsx(c,{children:"TV"})]}),o.jsxs(m,{$active:h==="Shower/WC",children:[o.jsx(d,{children:o.jsx(J,{})}),o.jsx(c,{children:"Shower/WC"})]})]})]}),o.jsxs(O,{children:[o.jsx(E,{children:"Vehicle type"}),o.jsx(H,{}),o.jsxs(ce,{onClick:T,children:[o.jsxs(m,{$active:i==="Van",children:[o.jsx(d,{children:o.jsx(k,{})}),o.jsx(c,{children:"Van"})]}),o.jsxs(m,{$active:i==="Fully Integrated",children:[o.jsx(d,{children:o.jsx(k,{})}),o.jsx(c,{children:"Fully Integrated"})]}),o.jsxs(m,{$active:i==="Alcove",children:[o.jsx(d,{children:o.jsx(k,{})}),o.jsx(c,{children:"Alcove"})]})]})]})]}),o.jsx(pe,{children:"Search"})]})}const ue=r.button`
+  border: none;
+  outline: none;
+  background: none;
+  border: 1px solid ${e=>e.theme.colors.inputBgColor};
+  border-radius: 20rem;
+  padding: 1.6rem 3.2rem;
+  width: 145px;
+  height: 56px;
+
+  font-family: ${s};
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 1.42857;
+  color: ${e=>e.theme.colors.mainTextColor};
+  background-color: ${e=>e.theme.colors.headerBgColor};
+
+  transition: all var(--primary-transition);
+
+  &:hover {
+    cursor: pointer;
+    border: 1px solid ${e=>e.theme.colors.accentColorHover};
+    /* background-color: ${e=>e.theme.colors.accentColorHover}; */
+    /* color: ${e=>e.theme.colors.white}; */
+  }
+`;function ge({onLoadMoreClick:e}){return o.jsx(ue,{onClick:e,children:"Load More"})}function je(){const e=q(),x=I(Q),[L,y]=a.useState(!1),[p,u]=a.useState(1),[C,g]=a.useState(!0),[h,f]=a.useState([]),i=I(U);function b(){u(t=>t+1)}function $(){u(1)}const w=Math.ceil(13/4);a.useEffect(()=>{async function t(){try{g(p<w),await e(N(p)).unwrap()}catch(l){console.log(l.message)}}t()},[e,p,w]);function v(){return h.filter(l=>{const A=!i.brand||l.make===i.brand,P=parseInt(l.rentalPrice.replace("$",""))<=i.price,V=!i.mileageRange||l.mileage>=i.mileageRange.min&&l.mileage<=i.mileageRange.max;return A&&P&&V})}const n=v();return o.jsxs(o.Fragment,{children:[o.jsx(G,{children:o.jsxs(K,{children:[o.jsx("title",{children:"Cruise Wheels - Explore the Catalog"}),o.jsx("meta",{name:"description",content:"Browse through our diverse catalog of stylish and comfortable cars at Cruise Wheels. Find the perfect vehicle for your next adventure and experience the joy of premium car rentals."}),o.jsx("meta",{name:"keywords",content:"car catalog, car rental, stylish cars, comfortable journeys, Cruise Wheels, premium rental services"})]})}),o.jsxs(X,{children:[o.jsx(xe,{handlePage:$,setDisplayedCars:f,setFilteredSearch:y}),o.jsxs(Y,{children:[o.jsx(_,{children:n&&(n==null?void 0:n.length)===0&&L?o.jsx(ee,{children:"No matching cars found"}):(n==null?void 0:n.length)>0?n==null?void 0:n.map(t=>o.jsx(z,{items:t},B())):x==null?void 0:x.map(t=>o.jsx(z,{items:t},B()))}),x.length>4&&o.jsx(ge,{onLoadMoreClick:b})]})]})]})}export{je as default};
