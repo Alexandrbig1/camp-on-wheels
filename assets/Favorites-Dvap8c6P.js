964d47ce19bbfded6d01bbf201c87ede352dcf44
Alexandrbig1 <alexsmagin1@gmail.com>
2024-04-19 14:39:16 -0500

    Updates

--- assets/Favorites-Dvap8c6P.js
+++ assets/Favorites-Dvap8c6P.js
@@ -1,4 +1,4 @@
-import{u as r,p as l,r as s,n as h,z as g,j as e,H as f,l as u,s as v}from"./index-CLYMN5kz.js";import{C as x,v as y}from"./v4-Bvxq41k2.js";import{h as j}from"./heroBg-BCOlliND.js";import"./emailRegex-TuPp45K5.js";const b=r.div`
+import{u as r,p as l,r as s,n as h,z as g,j as e,H as f,l as u,s as v}from"./index-Dae_NAH0.js";import{C as x,v as y}from"./v4-BrhIz4-f.js";import{h as j}from"./heroBg-BCOlliND.js";import"./emailRegex-D2pdpUGr.js";const b=r.div`
   display: flex;
   flex-direction: column;
   gap: 5rem;
