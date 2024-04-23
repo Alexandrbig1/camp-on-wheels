2fea1c6a68738f138e8de599966700176b6a3c88
Alexandrbig1 <alexsmagin1@gmail.com>
2024-04-23 13:22:21 -0500

    Updates

--- assets/Favorites-C92Wc_t5.js
+++ assets/Favorites-C92Wc_t5.js
@@ -1,4 +1,4 @@
-import{u as r,p as l,r as s,n as h,z as g,j as e,H as f,l as u,s as v}from"./index-AsOTuWuz.js";import{C as x,v as y}from"./v4-56XLfQmP.js";import{h as j}from"./heroBg-BCOlliND.js";import"./emailRegex-24BZTttZ.js";const b=r.div`
+import{u as r,p as l,r as s,n as h,z as g,j as e,H as f,l as u,s as v}from"./index-B9ZeogXV.js";import{C as x,v as y}from"./v4-xcvLfIh9.js";import{h as j}from"./heroBg-BCOlliND.js";import"./emailRegex-BafwMLXT.js";const b=r.div`
   display: flex;
   flex-direction: column;
   gap: 5rem;
